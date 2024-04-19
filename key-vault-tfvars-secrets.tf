0eac0b1e736b653094192e785e73b3fe52ed9ec9
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2024-04-19 15:20:12 +0100

    Updated Key Vault module to 0.4.0

--- key-vault-tfvars-secrets.tf
+++ key-vault-tfvars-secrets.tf
@@ -1,14 +1,15 @@
 module "azurerm_key_vault" {
-  source = "github.com/DFE-Digital/terraform-azurerm-key-vault-tfvars?ref=v0.3.0"
+  source = "github.com/DFE-Digital/terraform-azurerm-key-vault-tfvars?ref=v0.4.0"
 
-  environment                           = local.environment
-  project_name                          = local.project_name
-  existing_resource_group               = module.azurerm_logic_app_workflow.resource_group.name
-  azure_location                        = local.azure_location
-  key_vault_access_users                = local.key_vault_access_users
-  key_vault_access_ipv4                 = local.key_vault_access_ipv4
-  tfvars_filename                       = local.tfvars_filename
-  diagnostic_log_analytics_workspace_id = module.azurerm_logic_app_workflow.azurerm_log_analytics_workspace.id
+  environment                             = local.environment
+  project_name                            = local.project_name
+  existing_resource_group                 = module.azurerm_logic_app_workflow.resource_group.name
+  azure_location                          = local.azure_location
+  key_vault_access_use_rbac_authorization = true
+  key_vault_access_users                  = []
+  key_vault_access_ipv4                   = local.key_vault_access_ipv4
+  tfvars_filename                         = local.tfvars_filename
+  diagnostic_log_analytics_workspace_id   = module.azurerm_logic_app_workflow.azurerm_log_analytics_workspace.id
 
   tags = local.tags
 }
